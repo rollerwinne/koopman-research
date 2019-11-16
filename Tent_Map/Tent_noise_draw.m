4e68813068d3e93886ff43ffcac51066beba3eb0
zhangcong-mbp <502358401@qq.com>
2019-11-17 07:02:22 +0800

    update Tent_noise_draw

--- Tent_Map/Tent_noise_draw.m
+++ Tent_Map/Tent_noise_draw.m
@@ -2,80 +2,150 @@
 % 测试数据
 clear;close all
 n=1000;%粒子个数
-m=20;%基函数个数 6 100;100 10
-times=60;%一次演化波包数
-D=0.1;%噪声强度,信噪比的倒数
+m=100;%基函数个数 6 100;100 10
+times=10;%一次演化波包数
+D=0.001;%噪声强度,信噪比的倒数
+
 
 x0=linspace(0,1,n);
-f=@(x)awgn(1-2*abs(x-1/2),10*log10(1/D)); %Tent map with noise
-% g=@(x)1-2*abs(x-1/2); %Tent map
-% f=@(x)awgn(g(g(x)),10*log10(1/D)); % Tent map*2
-% f=@(x)awgn(3.90.*x.*(1-x),10*log10(1/D)); %Logistic map with noise
-% f=@(x)4.*x.*(1-x);%Logistic map
+%[f,seq,sx]=Tents_function(7,D);           % Tents map
+[f,seq,sx]=Tents_function_low(3,D);           % Tents map low
+% f=@(x)awgn(1-2*abs(x-1/2),10*log10(1/D)); % Tent map with noise
+% f=@(x)1-2*abs(x-1/2);                     % Tent map
+% f=@(x)awgn(g(g(x)),10*log10(1/D));        % Tent map*2
+% f=@(x)awgn(3.90.*x.*(1-x),10*log10(1/D)); % Logistic map with noise
+% f=@(x)4.*x.*(1-x);                        % Logistic map
 % f=@(x)awgn(2.5980762113533159402911695122588.*x.*(1-x).*(2-x),10*log10(1/D)); %偏移至0.41左右 with noise
 % f=@(x)2.5980762113533159402911695122588.*x.*(1-x).*(2-x); %偏移至0.41左右
-figure(3);plot(x0,f(x0))
-X=zeros(n,times);
-
-K=zeros(n,m);
-L=zeros(n,m);
-for j=1:m %对于每个基函数
-%     g_temp=Rect_fun(j,m);%rect基函数
-    g_temp=Gauss_fun(m);%Gauss基函数
-    for i=1:length(x0) %对于每个相空间的点
-        K(i,j)=g_temp(x0(i),j,m);
-        x_temp=[];
-        for k=1:times %每个点演化迭代times次
-            x_temp=[x_temp,g_temp(f(x0(i)),j,m)];
+s=jet(n);
+for m=[4,6,8,10,12,14,16,20,28,32,38,44,50,60,64,72,80,90,100]
+    figure(m);
+    subplot(3,3,1)
+    plot(x0,f(x0))
+    if exist('seq')
+        hold on
+        plot(seq,0,'r*')
+    end
+    X=zeros(n,times);
+    
+    K=zeros(n,m);
+    L=zeros(n,m);
+    for j=1:m %对于每个基函数
+        %     g_temp=Rect_fun(j,m);%rect基函数
+        g_temp=Gauss_fun(m);%Gauss基函数
+        for i=1:length(x0) %对于每个相空间的点
+            K(i,j)=g_temp(x0(i),j,m);
+            x_temp=[];
+            for k=1:times %每个点演化迭代times次
+                x_temp=[x_temp,g_temp(f(x0(i)),j,m)];
+            end
+            %x_temp=x_temp(x_temp<=1 & x_temp>=0);
+            L(i,j)=sum(x_temp)/times;
         end
-        %x_temp=x_temp(x_temp<=1 & x_temp>=0);
-        L(i,j)=sum(x_temp)/times;
+    end
+    U=pinv(K)*L;
+    U=U;
+    [F,D]=eig(U);
+    D=diag(D);
+    h=find(abs(D)>0.01 & abs(D)<1.3 & imag(D)>-1e-6);
+    if length(D)<=9
+        h=1:length(D);
+    end
+    figure(m);
+    for i=1:min(length(h),8)
+        A=real(K*F(:,h(i)));
+        subplot(3,3,i+1)
+        % set(gcf,'outerposition',get(0,'screensize'));
+        hh=plot(x0,A);
+        hold on
+        % draw_peaks(A,x0,seq,sx);
+        % draw_trough(A,x0,seq,sx);
+        [xp,yp]=draw_peaks(A,x0);
+        [xt,yt]=draw_trough(A,x0);
+        if (i==1)
+            xpp=xp;
+            ypp=yp;
+            xtt=xt;
+            ytt=yt;
+            AA=A;
+        end
+        if exist('seq')
+            plot(seq,min(A),'r*')
+        end
+        if exist('sx')
+            draw_boundary(min(A),max(A),sx)
+        end
+        d_abs=abs(D(h(i)));
+        d_angle=angle(D(h(i)))/pi*180;
+        str1=['n=',num2str(n),'; m=',num2str(m)];
+        str2=[num2str(d_abs) ' ∠' num2str(d_angle) '°'];
+        title({str1;str2});
+    end
+    figure(101)
+    subplot(121)
+    hold on
+    for i=1:length(ypp)
+        y1index=floor( (ypp(i)-min(AA)) / (max(AA)-min(AA))*n+1);
+        if y1index>n
+            y1index=n;
+        end
+        plot(m,xpp(i),'*','Color',s(y1index,:))
+    end
+    subplot(122)
+    hold on
+    for i=1:length(ytt)
+        y2index=floor( (ytt(i)-min(AA)) / (max(AA)-min(AA))*n+1);
+        if y2index>n
+            y2index=n;
+        end
+        plot(m,xtt(i),'*','Color',s(y2index,:))
     end
 end
-U=pinv(K)*L;
-U=U;
-[F,D]=eig(U);
-D=diag(D);
-h=find(abs(D)>0.01 & abs(D)<1.3 & imag(D)>-1e-6);
-if length(D)<=9
-    h=1:length(D)
-end
-for i=1:min(length(h),9)
-    A=real(K*F(:,h(i)));
-    figure(1);
-    subplot(3,3,i)
-    %set(gcf,'outerposition',get(0,'screensize'));
-    hh=plot(x0,A);
-    d_abs=abs(D(h(i)));
-    d_angle=angle(D(h(i)))/pi*180;
-    str1=['n=',num2str(n),'; m=',num2str(m)];
-    str2=[num2str(d_abs) ' ∠' num2str(d_angle) '°'];
-    title({str1;str2});
-end
-figure(2);
-SVG_draw(U,K,n,m,0,0.1,1)
-% figure(3);
-% subplot(131);spyl(U);colorbar;title('U')
-% subplot(132);spyl(K);colorbar;title('K')
-% subplot(133);spyl(L);colorbar;title('L')%K与L的矩阵形式
+colormap(jet)
+%figure(3);
+%SVG_draw(U,K,n,m,0,0.1,1)
 
-%
-% for i=1:length(x0)
-%     g_temp=Tent_Rect_fun(i,m);
-%     for j=1:times
-%         X(i,j)=f(x0(i));
-%     end
-% end
-% X=X(:);
-% X=X(X<=1 & X>=0);
-% rou=zeros(1,n);
-% roux=0:1/n:1-1/n;
-% for i=1:n
-%     rou(i)=sum((X>=roux(i))&(X<roux(i)+1/n));
-% end
-% rou(n)=rou(n)+sum(X==1);
-% % rou=rou/times;
-% end
+function [xt,yt]=draw_trough(A,x0,seq,sx)
+[pks,locs] = findpeaks(-A);
+xt=x0(locs);
+yt=A(locs);
+plot(xt,yt,'s');
+hold on
+if (nargin>2)
+    [m,n]=size(sx);
+    combine=[reshape(seq,length(seq),1);reshape(sx,m*n,1)];
+    for i=1:length(xt)
+        minindex=find(abs(abs(xt(i)-combine)) - min(abs(xt(i)-combine))<1e-10)
+        diff=combine(minindex(1))-xt(i)
+        text(xt(i),yt(i),num2str(abs(diff),'%5.4f'),'VerticalAlignment','bottom','rotation',90);
+    end
+end
+end
+
+function [xp,yp]=draw_peaks(A,x0,seq,sx)
+[pks,locs] = findpeaks(A);
+xp=x0(locs);
+yp=A(locs);
+plot(xp,yp,'s');
+hold on
+if (nargin>2)
+    [m,n]=size(sx);
+    combine=[reshape(seq,length(seq),1);reshape(sx,m*n,1)];
+    for i=1:length(xp)
+        minindex=find(abs(abs(xp(i)-combine)) - min(abs(xp(i)-combine))<1e-10)
+        diff=combine(minindex(1))-xp(i)
+        text(xp(i),yp(i),num2str(abs(diff),'%5.4f'),'VerticalAlignment','bottom','rotation',90);
+    end
+end
+end
+
+function draw_boundary(min,max,sx)
+l=length(sx(:,1));
+s=cool(l);
+for i=1:l
+    plot(sx(i,:),(max-min)/l*i+min,'*','color',s(i,:));
+end
+end
 
 function g = Rect_fun(i,m)
 % x is a number
---
+++ Tent_Map/Tents_function.m
@@ -0,0 +1,36 @@
+function [f,seq,sx]=Tents_function(n,D)
+seq=linspace(0,1,n);
+block=seq(2);
+k=1/seq(2);
+str='@(x)';awgnstr=str;
+if (nargin>1)
+    awgnstr=[awgnstr,'awgn('];
+end
+for i=1:n-1
+    if (i==n-1) %最后一个取双闭区间
+        area=['.*(x>=',num2str(i-1),'*block&x<=',num2str(i),'*block)'];
+    else
+        area=['.*(x>=',num2str(i-1),'*block&x<',num2str(i),'*block)'];
+    end
+    if(mod(i,2)==1) %奇偶表达式不同
+        equ=['(-',num2str(i-1),'+k*x)'];
+    elseif mod(i,2)==0
+        equ=['(',num2str(i),'-k*x)'];
+    else
+        disp('error');
+    end
+    str=[str,'+',equ,area];
+    awgnstr=[awgnstr,'+',equ,area];
+end
+if(nargin>1)
+    awgnstr=[awgnstr,',10*log10(1/D))'];
+end
+disp(awgnstr)
+disp(str)
+f=eval(awgnstr);
+x0=seq(1:end-1)+block/2;
+sx=[];
+for i=2:n-1
+    sx=[sx;fsolve(eval([str,'-',num2str(seq(i))]),x0)];
+end
+end
